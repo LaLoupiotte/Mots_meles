f1fbe8e6bcb6942d19b50774d834f1cb62acb87d
LaLoupiotte <74879238+LaLoupiotte@users.noreply.github.com>
2022-12-05 15:46:24 +0100

    creation plateau.cs

--- Mots_meles/Dictionnaire.cs
+++ Mots_meles/Dictionnaire.cs
@@ -3,10 +3,57 @@ namespace Mots_meles
 {
     public class Dictionnaire
     {
+        private int longueur;
+        private string langue;
+        private string[] dico;
 
-
-        public Dictionnaire()
+        public Dictionnaire(int longueur, string langue)
         {
+            this.longueur = longueur;
+            this.langue = langue;
+
+        }
+
+        public override string ToString()
+        {
+            string res = "longueur : " + this.longueur + " langue : " + this.langue;
+            return res;
+        }
+
+        public bool RechDichoRecursif(string mot)
+        {
+            //IL FAUT TROUVER UN MOYEN DE NE PAS CHANGER LE TABLEAU THIS.DICO
+
+
+
+            //Test si le dictionnaire est vide
+            if(dico.Length == 0 || dico == null)
+            {
+                return false;
+            }
+            //Teste si le premier élement du tableau est le bon mot
+            else if(dico[0] == mot)
+            {
+                return true;
+            }
+            //Si ce n'est pas le bon mot on rappelle la fonction RechDichoRecursif
+            //avec le même tableau mais qui commence à l'indice un
+            else
+            {
+                //Création d'un nouveau dictionnaire commencant à l'indice un
+                string[] newDico = new string[dico.Length];
+                for(int i = 1; i < dico.Length; i++)
+                {
+                    newDico[i - 1] = dico[i];
+                }
+                //On copie le nouveau tableau dans this.dico
+                dico = new string[newDico.Length];
+                for(int i = 0; i < newDico.Length; i++)
+                {
+                    dico[0] = newDico[0];
+                }
+                return RechDichoRecursif(mot);
+            }
         }
     }
 }
---
+++ Mots_meles/Plateau.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace Mots_meles
+{
+    public class Plateau
+    {
+        private int difficulte;
+        private int nbColonnes;
+        private int nbLignes;
+        private string[] mots;
+        private string[] directions = new string[] { "N", "S", "E", "O", "NE", "NO", "SE", "SO" };
+
+        
+
+        public Plateau()
+        {
+            //Constructeur par default avec ces mots par defauts
+            //On utilise se constructeur en tant que brouillon
+            mots = new string[] {"bonjour", "elysee", "chaussures", "automatismes", "toiture"};
+            nbColonnes = 10;
+            nbLignes = 15;
+            string[,] matriceMotsMeles = matriceFromMots(mots);
+        }
+
+        private string[,] matriceFromMots(string[] mots)
+        {
+            int conteurMots = 0;
+            while(conteurMots < mots.Length)
+            {
+                var rand = new Random();
+                int colonne = rand.Next(nbColonnes);
+                int ligne = rand.Next(nbLignes);
+                string direction = directions[rand.Next(directions.Length)];
+            }
+        }
+
+        private string printMatrice(int[,] matrice)
+        {
+            string res = "";
+            for (int i = 0; i < matrice.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrice.GetLength(1); j++)
+                {
+                    res += matrice[i, j] + ",";
+                }
+                res += "\n";
+            }
+            return res;
+        }
+
+    }
+}
