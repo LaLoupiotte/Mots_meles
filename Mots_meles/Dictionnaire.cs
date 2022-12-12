c9660bc64ca6261c8f6e003599e6585be88d0dd5
LaLoupiotte <74879238+LaLoupiotte@users.noreply.github.com>
2022-12-12 21:32:03 +0100

    Plateau.cs qui marche

--- Mots_meles/Dictionnaire.cs
+++ Mots_meles/Dictionnaire.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace Mots_meles
 
 /*
@@ -39,12 +43,18 @@ namespace Mots_meles
     {
         private int difficulte;
         private string langue; 
-        private string[] dico;
+        private List<string> words;
+
+        public List<string> Words
+        {
+            get { return this.words; }
+        }
 
         public Dictionnaire(int difficulte, string langue)
         {
             this.difficulte = difficulte;
             this.langue = langue;
+            this.words = ReadFile("fichiersTexte/MotsPossiblesFR.txt", difficulte);
         }
 
         public override string ToString()
@@ -61,21 +71,44 @@ namespace Mots_meles
                     return false;
                 }
                 int start = 0;
-                int end = this.dico.Length - 1;
+                int end = this.words.Count - 1;
                 int middle;
 
                 while (start <= end)
                 {
                     middle = (start + end) / 2;
-                    if (this.dico[middle] == mot)
+                    if (this.words[middle] == mot)
                         return true;
-                    else if (string.Compare(mot, this.dico[middle]) < 0)
+                    else if (string.Compare(mot, this.words[middle]) < 0)
                         end = middle - 1;
                     else
                         start = middle + 1;
                 }
                 return false;
             }
+        }
+
+        public static List<string> ReadFile(string filePath, int difficulte)
+        {
+            ArrayList lines = new ArrayList();
+            try
+            {
+                string text = File.ReadAllText(filePath);
+                lines.AddRange(text.Split('\n'));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            int lineOfDifficulte = (difficulte-1)*2-1;
+            string[] line = lines[lineOfDifficulte ].ToString().Split(" ");
+            List<string> words = new List<string>();
+            for (int j = 0; j < line.Length; j++)
+            {
+                words.Add(line[j]);
+            }
+            return words;
         }
     }
 }
