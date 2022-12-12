01f88563b0ae94f6453cdc7940a5beb13346968e
NonoInge <104363390+NonoInge@users.noreply.github.com>
2022-12-12 18:04:53 +0100

    modif jeu

--- Mots_meles/Jeu.cs
+++ Mots_meles/Jeu.cs
@@ -3,6 +3,43 @@ namespace Mots_meles
 {
 	public class Jeu
 	{
+		public Jeu()
+		{
+            //déclarations
+            int nb_joueur = 0;
+            Joueur[] tab_joueurs = new Joueur[nb_joueur];
+            string langue_dic = null;
+            string nom_temp = null;
 
-	}
+            //Affichage menu + saisie des caractéristiques des joueurs 
+            Console.WriteLine("=-=-=-=-=-= MOTS MELES DEVINCI =-=-=-=-=-=\n");
+            do
+            {
+                Console.WriteLine("\nInserer le nombre de joueurs ");
+                nb_joueur = int.Parse(Console.ReadLine()); //saisie par le joueur du nb de joueurs
+            } while (nb_joueur < 0 || nb_joueur == 0);
+
+            Console.WriteLine("Vous avez selectionne " + nb_joueur + " joueurs");
+
+            for (int i = 0; i < nb_joueur; i++) //remplissage du tableau de joueur
+            {
+                Console.WriteLine("\nSaisir le nom du joueur " + (i + 1));
+                nom_temp = Console.ReadLine();
+                Console.WriteLine(nom_temp);
+
+                //tab_joueurs[i] = new Joueur(nom_temp);
+            }
+
+            do
+            {
+                Console.WriteLine("Saisir une langue : \nFR : Francais\nAN : Anglais"); //saisie de la langue du dico
+                langue_dic = Console.ReadLine();
+            } while (langue_dic != "FR" || langue_dic != "AN");
+
+
+
+            ///LANCEMENT D UNE PARTIE
+            ///
+        }
+    }
 }
--- Mots_meles/Program.cs
+++ Mots_meles/Program.cs
@@ -6,36 +6,7 @@ namespace Mots_meles
     {
         static void Main(string[] args)
         {
-            //déclarations
-            int nb_joueur=0;
-            Joueur[] tab_joueurs;
-            string langue_dic;
-
-            //Affichage menu + saisie des caractéristiques des joueurs 
-            Console.WriteLine(" =-=-=-=-=-= MOTS MELES DEVINCI =-=-=-=-=-=\n");
-            do { 
-                Console.WriteLine("\n Inserer le nombre de joueurs ");
-                nb_joueur=int.Parse(Console.ReadLine()); //saisie par le joueur du nb de joueurs
-            }while(nb_joueur<0||nb_joueur==0);
-
-            Console.WriteLine("\n Vous avez selectionne " + nb_joueur + "joueurs");
-            
-            for(int i=0; i<nb_joueur; i++) //remplissage du tableau de joueur
-            {
-                Console.WriteLine("\n\n Saisir le nom du joueur " + (i+1));
-                tab_joueurs[0]=new Joueur(Console.ReadLine());
-            }
-
-            do
-            {
-                Console.WriteLine(" Saisir une langue : \n FR : Francais\n AN : Anglais"); //saisie de la langue du dico
-                langue_dic=Console.ReadLine();
-            }while(langue_dic!="FR" || langue_dic!="AN");
-            
-
-
-            ///LANCEMENT D UNE PARTIE
-            ///
+            Jeu jeu = new Jeu();
 
         }
     }
