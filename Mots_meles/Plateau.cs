20019eed30c3bc4420bee3efe853cbf99c13c014
LaLoupiotte <74879238+LaLoupiotte@users.noreply.github.com>
2022-12-13 08:44:55 +0100

    Comment on plateau.cs

--- Mots_meles/Plateau.cs
+++ Mots_meles/Plateau.cs
@@ -9,7 +9,7 @@ namespace Mots_meles
     public class Plateau
     {
         private int difficulte;
-        private string[] words;
+        private List<string> words;
         private char[,] grid;
         private string langue;
         private string[] directions;
@@ -25,12 +25,16 @@ namespace Mots_meles
         {
             this.difficulte = difficulte;
             this.langue = langue;
-            List<string> words = new List<string>();
-            for (int i = 2; i < 15; i++)
+            //Création d'une collection liste qui va stocker tout les mots de la langue this.langue
+            words = new List<string>();
+            //La boucle va de 2 à 15 car cela correspond a la taille des différents mots. Ici on les veut tous
+            for (int longeur = 2; longeur < 16; longeur++)
             {
-                words.AddRange(new Dictionnaire(i, langue).Words);
+                //Ajout a la liste addrange de la liste words du dictionnaire correspodant à la longeur et a langue désirée
+                words.AddRange(new Dictionnaire(longeur, langue).Words);
             }
-            
+
+            //On fait une switch pour affecter le bon tableau a this.directions et une taille de plus en plus grande aux grille
             switch (difficulte)
             {
                 case 1:
@@ -54,16 +58,19 @@ namespace Mots_meles
                     this.width = 21;
                     break;
                 default:
+                    //Par défaut nous avons décidé de mettre la valeur maximale même si ce cas la ne sera jamais traité 
+                    //il permet d'éviter une erreur de compilation lors des test 
                     this.directions = new string[] { "S", "O", "N", "E", "SO", "NE", "NO", "SE" };
                     this.heigth = 16;
                     this.width = 21;
                     break;
             }
+            //Création de la graille avec la bonne hauteur et largeur
             this.grid = new char[this.heigth, this.width];
 
-            
 
-            // Initialise the grid with empty spaces
+
+            //Initialisation de la grille avec des espaces vides
             for (int i = 0; i < heigth; i++)
             {
                 for (int j = 0; j < width; j++)
@@ -71,29 +78,31 @@ namespace Mots_meles
                     grid[i, j] = ' ';
                 }
             }
-
+        }
+        public void GenereGrille() {
             // Randomly insert words
             Random rnd = new Random();
             int cont = 0;
             while (cont < 10)
             {
-                
-                string word = words[rnd.Next(0, words.Count-1)];
+                //Choisit un mot, une position (x, y) et une direcetions aléatoire
+                string word = words[rnd.Next(0, words.Count - 1)];
                 int x = rnd.Next(0, width);
                 int y = rnd.Next(0, heigth);
+                //Ici on génère aléatoirement un nombre en fonction de la longeur du tableau directions car il change
+                //en fonction de la difficulté
                 int directionIndice = rnd.Next(0, directions.Length);
-
-                // Check if the word fits in the grid
-                if (CheckWord(x, y, word, this.directions[directionIndice], grid, width, heigth))
+                //Teste si le mot peut etre inserre dans la grille
+                //J'utilise cette méthode pour ne pas avoir a tout supprimer si disons le 5eme lettre ne rentre pas.
+                if (Disponible(x, y, word, this.directions[directionIndice], grid, width, heigth))
                 {
-                    InsertWord(x, y, word, this.directions[directionIndice], grid);
+                    //Si disponible alors on inserre le mot
+                    Inserre(x, y, word, this.directions[directionIndice], grid);
                     Console.WriteLine("Word '{0}' starts at {1}, {2} and goes {3}", word, x, y, directions[directionIndice]);
                     cont += 1;
                 }
-
             }
-
-            // Fill the empty spaces with random characters
+            //Remplir les espaces vides avec des characteres aléatoires
             for (int i = 0; i < heigth; i++)
             {
                 for (int j = 0; j < width; j++)
@@ -104,14 +113,16 @@ namespace Mots_meles
                     }
                 }
             }
-
         }
 
-        public void InsertWord(int x, int y, string word, string direction, char[,] grid)
+        public void Inserre(int x, int y, string word, string direction, char[,] grid)
         {
+            //Boucle qui traite chaque charactere du mot
             for (int i = 0; i < word.Length; i++)
             {
+                //On remplit la case par le charactere souhaité
                 grid[x, y] = char.ToUpper(word[i]);
+                //On fait un switch qui en fonction de la direction change les valeurs des coordonnées (x, y)
                 switch (direction)
                 {
                     // Est
@@ -154,18 +165,24 @@ namespace Mots_meles
             }
         }
 
-        public bool CheckWord(int x, int y, string word, string direction, char[,] grid, int width, int height)
+        public bool Disponible(int x, int y, string word, string direction, char[,] grid, int width, int height)
         {
+            //Boucle qui traite chaque charactere du mot
             for (int i = 0; i < word.Length; i++)
             {
+                //Ce if nous permet de savoir si le mot rentre dans la grille
                 if (x < 0 || x > height-1 || y < 0 || y > width-1)
                 {
                     return false;
                 }
-                if (grid[x, y] != ' ' && grid[x, y] != word[i])
+                //Ce else if nous permet de savoir si la position dans la grille est vide afin de la remplacer par le 
+                //charactere word[i]. Si la case est occupé par le même charactere que word[i] la condition reste True
+                //car sinon les mots ne peuvent pas se "croisés" dans la grille 
+                else if (grid[x, y] != ' ' && grid[x, y] != word[i])
                 {
                     return false;
                 }
+                //On fait un switch qui en fonction de la direction change les valeurs des coordonnées (x, y)
                 switch (direction)
                 {
                     // Est
